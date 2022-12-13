61d52617660b69f1a0344a4605a956037fec97a3
Kristjan Eimre <kristjaneimre@gmail.com>
2022-12-13 16:57:41 +0200

    add Materials Cloud header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 import ACWF from "./components/ACWF";
+import MaterialsCloudHeader from "react-materialscloud-header";
 
 function App() {
   return (
-    <div className="App">
-      <ACWF />
-    </div>
+    <MaterialsCloudHeader
+      activeSection={"discover"}
+      breadcrumbsPath={[
+        { name: "Discover", link: "https://www.materialscloud.org/discover" },
+        { name: "ACWF verification", link: null },
+      ]}> 
+      <div className="App">
+        <ACWF />
+      </div>
+    </MaterialsCloudHeader> 
   );
 }
 

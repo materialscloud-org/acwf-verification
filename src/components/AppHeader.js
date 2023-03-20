5a490ce3af434edb1c29cfe55c2094b121e9a4fd
Kristjan Eimre <kristjaneimre@gmail.com>
2023-03-20 13:30:23 +0200

    add preliminary DOI badge

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -2,10 +2,17 @@ import React from "react";
 
 import "./AppHeader.css";
 
+import DoiBadge from "./DoiBadge";
+
 export default function AppHeader() {
   return (
     <div className="title-and-logo">
-      <span>AiiDA common workflows verification</span>
+      <div className="title-and-doi">
+        <span className="title-span">AiiDA common workflows verification</span>
+        <div style={{ marginLeft: "4px" }}>
+          <DoiBadge />
+        </div>
+      </div>
       <img src="./acwf-logo.png" className="acwf-logo"></img>
     </div>
   );
